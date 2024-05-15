5a5078675485a7146c01ce8cf1d1aeda542f732d
Dmytro <91199246+dim4ik9228@users.noreply.github.com>
2024-05-15 16:32:05 +0300

    Add files via upload

---
+++ ConsoleApp1/VideoEquipmentDecorator.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1;
+
+internal class VideoEquipmentDecorator(ConferenceRoomComponent component) : ConferenceRoomDecorator(component)
+{
+    public override string Description => $"{Component.Description}, Відео обладнання";
+
+    public override double Cost => Component.Cost + 1000.0;
+}
